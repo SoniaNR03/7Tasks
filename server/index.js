07d1dc0fe17c02b44ad31bac0aa66decca555fc0
sonia <sonianr03@gmail.com>
2024-08-03 12:32:23 +0200

    feat: setUp database with MongoDB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const { connectToMongoDB } = require('./database');
+
 
 const app = express();
+app.use(express.json());
 
 const router = require('./routes');
 
 // all routes start with /api 
 app.use('/api', router);
 // TODO: select a port
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; // 5000 is the default port in case env doesnt work
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+    await connectToMongoDB();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
+startServer();
--- server/routes.js
+++ server/routes.js
@@ -1,26 +1,68 @@
 const express = require("express");
-
 const router = express.Router();
 
+const { getConnectedClient } = require("./database");
+const { ObjectId } = require("mongodb");
+
+const getCollection = () => {
+    const client = getConnectedClient();
+    return client.db("todosdb").collection("todos");
+
+}
+
 // GET
-router.get("/todos", (req, res) => {
-    res.status(200).json({ msg: "GET REQUEST TO /api/todos" });
+router.get("/todos", async (req, res) => {
+    const collection = getCollection();
+    const todos = await collection.find({}).toArray();
+    res.status(200).json(todos);
 });
 
 // POST
-router.post('/todos', (req, res) => {
+router.post('/todos', async (req, res) => {
     // 201 creation of something
-    res.status(201).json({ msg: 'POST REQUEST TO /api/todos' });
+    const collection = getCollection();
+    const { todo } = req.body; // extract the todo from the request body(the property todo of the json format)
+
+    if (!todo) {
+        return res.status(400).json({ error: "No ToDo Found" });
+    }
+
+    todo = JSON.stringify(todo);
+
+    const newTodo = await collection.insertOne({ todo, status: false });
+
+
+    res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
 });
 
 // PUT
-router.put('/todos/:id', (req, res) => {
-    res.status(200).json({ msg: 'PUT REQUEST TO /api/todos/:id' });
+router.put('/todos/:id', async (req, res) => {
+    const collection = getCollection();
+    const _id = new ObjectId(req.params.id);
+    const { status, todo } = req.body;
+
+    if (typeof status !== 'boolean') {
+        return res.status(400).json({ error: "Invalid Status, Not Boolean" });
+    } else if (!todo || typeof todo !== 'string') {
+        return res.status(400).json({ error: "ToDo Not valid" });
+    }
+    const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status, todo } });
+    res.status(200).json(updatedTodo);
+
+
+
+
+
 });
 
 // DELETE
-router.delete('/todos/:id', (req, res) => {
-    res.status(200).json({ msg: 'DELETE REQUEST TO /api/todos/:id' });
+router.delete('/todos/:id', async (req, res) => {
+    const collection = getCollection();
+    const _id = new ObjectId(req.params.id);
+    const deletedTodo = await collection.deleteOne({ _id });
+
+
+    res.status(200).json(deletedTodo);
 });
 
 module.exports = router;
